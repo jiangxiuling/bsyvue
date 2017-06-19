43eefb5017b5fc6d722e31c825fe19238713a3db
jiangxiuling <787708113@qq.com>
2017-06-19 21:37:36 +0800

    第一次提交代码

---
+++ src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <div class="red">ewrrewr</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .red {
+    color: red;
+  }
+</style>
